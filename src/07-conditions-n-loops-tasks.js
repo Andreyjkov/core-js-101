251c813fca8d39dc009bf913f46d61c56c23185b
Andreyjkov <Andreyjskov@gmail.com>
2022-07-25 19:56:31 +0300

    fix:  getIntervalString task

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -202,11 +202,8 @@ function findFirstSingleChar(str) {
  *   5, 3, true, true   => '[3, 5]'
  *
  */
-function getIntervalString(a, b, isStartIncluded, isEndIncluded) {
-  const start = (isStartIncluded) ? '[' : '(';
-  const num = (a < b) ? `${a}, ${b}` : `${b}, ${a}`;
-  const end = (isEndIncluded) ? ']' : ')';
-  return `${start}${num}${end}`;
+function getIntervalString(/* a, b, isStartIncluded, isEndIncluded */) {
+  throw new Error('Not implemented');
 }
 
 
